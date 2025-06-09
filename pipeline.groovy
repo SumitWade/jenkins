1f56e368b67db33ca5e2de8ab07cdd57b48167d8
SumitWade <sumitwade1111@gmail.com>
2025-06-09 19:29:07 +0530

    deploy done

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,35 +1,45 @@
 pipeline {
-    agent {label 'node'}
+    agent { label 'node' }
+
     stages {
         stage('git_checkout') {
             steps {
-               git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
             }
         }
+
         stage('build') {
             steps {
                 sh '/opt/maven/bin/mvn clean package'
             }
         }
+
         stage('test') {
             steps {
-               withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cread') {
-               sh '/opt/maven/bin/mvn sonar:sonar'
-           }
-                // sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://43.201.115.47:9000 -Dsonar.login=1c03dfa37d9ca3815be1d37b1681fcd05b2d6d7b'
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cread') {
+                    sh '/opt/maven/bin/mvn sonar:sonar'
+                }
             }
         }
-          //      stage('Quality_Gate') {
-          //  steps {
-          //     timeout(2) {
-              
-         //   }
-            //   waitForQualityGate true
-          //  }
-        }
-                        stage('depoly') {
+
+        stage('Quality_Gate') {
             steps {
-               deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://35.179.105.163:8080/')], contextPath: '/', war: '**/* .war'
+                timeout(time: 2, unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+        }
+
+        stage('deploy') {
+            steps {
+                deploy adapters: [
+                    tomcat9(
+                        alternativeDeploymentContext: '', 
+                        credentialsId: 'tomcat-cred', 
+                        path: '', 
+                        url: 'http://35.179.105.163:8080/'
+                    )
+                ], contextPath: '/', war: '**/*.war'
             }
         }
     }
