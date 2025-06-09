a54542ee09f7928377c2baf97d222913830115d0
SumitWade <sumitwade1111@gmail.com>
2025-06-09 18:03:08 +0530

    build done

---
+++ pipeline.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent {label 'node'}
+    stages {
+        stage('git_checkout') {
+            steps {
+               git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+        stage('build') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+        stage('test') {
+            steps {
+               withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cread') {
+               sh '/opt/maven/bin/mvn sonar:sonar'
+           }
+                // sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://43.201.115.47:9000 -Dsonar.login=1c03dfa37d9ca3815be1d37b1681fcd05b2d6d7b'
+            }
+        }
+                stage('depoly') {
+            steps {
+               echo "deploy success"
+            }
+        }
+    }
+}
